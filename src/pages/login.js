9bbd8b71a25a644a1b114fa4dae41e7f4074a83d
Mark Francis Saldivar <mfrancissaldivar@gmail.com>
2022-03-27 01:37:45 +0800

    create a separate page for login, moved the meta name, meta content and favicon to _document.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,17 +1,16 @@
+import React from 'react'
 import Head from 'next/head'
 import { Header } from '../common/components/Header'
 import Login from '../common/components/Login/Login'
-import { WeatherCheck, WeatherResult } from '../modules/weather'
 
-export default function Home() {
+export default function login() {
   return (
     <div className='h-screen w-screen overflow-hidden'>
       <Head>
-        <title>Weather Forecast</title>
+        <title>Login - Weather Forecast</title>
       </Head>
       <Header />
-      <WeatherCheck />
-      <WeatherResult />
+      <Login />
     </div>
   )
 }
